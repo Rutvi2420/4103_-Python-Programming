f6bb7c37328039fe7453f544a172a2853bab1ba3
Rutvi Meghpara <meghpararutvi@gmail.com>
2026-02-14 10:42:04 +0530

    Add files via upload

---
+++ Fabonacci number.py
@@ -0,0 +1,13 @@
+# Program to find factorial of a number
+
+n = int(input("Enter a number: "))
+fact = 1
+
+if n < 0:
+    print("Factorial does not exist for negative numbers")
+elif n == 0:
+    print("Factorial of 0 is 1")
+else:
+    for i in range(1, n + 1):
+        fact *= i
+    print("Factorial of", n, "is:", fact)
